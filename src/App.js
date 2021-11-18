ccd48850e7194af582e4cadad0fdbae369e22d63
xJohnACx <61944045+xJohnACx@users.noreply.github.com>
2021-11-18 09:21:47 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Button from '@mui/material/Button'
 function App() {
   return (
     <Button variant="contained" color="primary">
-      Hola mundo John
+      Hola mundo John desde Lontec
     </Button>
   );
 }
